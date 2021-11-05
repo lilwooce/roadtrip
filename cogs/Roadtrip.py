d0baedd59770bf5b492f96fe4c7233ec791fed13
lilwooce <kryptstep@gmail.com>
2021-11-05 14:55:31 -0400

    debugging

--- cogs/Roadtrip.py
+++ cogs/Roadtrip.py
@@ -23,6 +23,8 @@ class Roadtrip(commands.Cog, name="Roadtrip"):
             obj = {"q1": ctx.author.id, "q2": song}
             result = requests.post(asurl, data=obj, headers={"User-Agent": "XY"})
             print(result.status_code)
+            print(result.url)
+            print(result.text)
             await ctx.channel.send(f"Added {song} to {ctx.author.name}'s playlist")
         except:
             await ctx.channel.send("Please input a valid song")
@@ -32,6 +34,8 @@ class Roadtrip(commands.Cog, name="Roadtrip"):
         try:
             obj = {"q1": ctx.author.id, "q2": song, "f1": "user", "f2": "song"}
             result = requests.post(rsurl, data=obj, headers={"User-Agent": "XY"})
+            print(result.status_code)
+            print(result.url)
             print(result.text)
             await ctx.channel.send(f"Removed {song} from {ctx.author.name}'s playlist")
         except:
