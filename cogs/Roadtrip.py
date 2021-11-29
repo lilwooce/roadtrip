d4944054a8dd5ff2da786664a47774ca8ce7d6a3
lilwooce <kryptstep@gmail.com>
2021-11-28 21:29:51 -0500

    Update Roadtrip.py

--- cogs/Roadtrip.py
+++ cogs/Roadtrip.py
@@ -116,7 +116,7 @@ class Roadtrip(commands.Cog, name="Roadtrip"):
         await ctx.voice_client.disconnect()
         await ctx.channel.send("left voice channel")
 
-    @commands.command()
+    '''@commands.command()
     async def play(self, ctx, url):
         channel = ctx.author.voice.channel
 
@@ -124,10 +124,11 @@ class Roadtrip(commands.Cog, name="Roadtrip"):
             await channel.connect()
             await ctx.channel.send("joined voice channel")
             
-            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-                file = ydl.extract_info(url, download=True)
-                guild = ctx.message.guild.id
-                path = str(file['title']) + "-" + str(file['id'] + ".mp3")
+            try:
+                with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                    ydl.download([url])
+            except:
+                os.system(f"""youtube-dl -o "song.%(ext)s" --extract-audio -x --audio-format mp3 {url}""")
 
             channelName = channel.name
             vc = await channel.connect()
@@ -142,7 +143,36 @@ class Roadtrip(commands.Cog, name="Roadtrip"):
                 await vc.disconnect() #and disconnects
                 print("Disconnected")
         else:
-            await ctx.send("Please join a voice channel")
+            await ctx.send("Please join a voice channel")'''
+
+    @commands.command(pass_context=True, brief="This will play a song 'play [url]'", aliases=['pl'])
+    async def play(ctx, url: str):
+        song_there = os.path.isfile("song.mp3")
+        try:
+            if song_there:
+                os.remove("song.mp3")
+        except PermissionError:
+            await ctx.send("Wait for the current playing music end or use the 'stop' command")
+            return
+        await ctx.send("Getting everything ready, playing audio soon")
+        print("Someone wants to play music let me get that ready for them...")
+        voice = ctx.author.voice
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+        }
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+        for file in os.listdir("./"):
+            if file.endswith(".mp3"):
+                os.rename(file, 'song.mp3')
+        voice.play(discord.FFmpegPCMAudio("song.mp3"))
+        voice.volume = 100
+        voice.is_playing()
 
 def setup(bot):
     bot.add_cog(Roadtrip(bot))
